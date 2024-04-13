46ad46de5a9a791290710a09e258f7f50dad1208
hayden <48267247+hayden-fr@users.noreply.github.com>
2024-04-13 22:12:51 +0800

    chore: Optimize bundle

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,11 @@
-import { unlinkSync } from 'node:fs'
+import { existsSync, unlinkSync } from 'node:fs'
 import { resolve } from 'node:path'
 
 const cwd = process.cwd()
-
-unlinkSync(resolve(cwd, 'dist/index.d.cts'))
-unlinkSync(resolve(cwd, 'dist/index.d.mts'))
+const unlinkFiles = ['index.d.cts', 'index.d.mts']
+for (const file of unlinkFiles) {
+  const filepath = resolve(cwd, 'dist', file)
+  if (existsSync(filepath)) {
+    unlinkSync(filepath)
+  }
+}
--- scripts/stub.ts
+++ scripts/stub.ts
@@ -1,7 +1,9 @@
-import { writeFileSync } from 'node:fs'
+import { readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 
 const cwd = process.cwd()
 
-const esmPath = 'dist/index.mjs'
-writeFileSync(resolve(esmPath), `export * from "${cwd}/src/index";\n`)
+writeFileSync(
+  resolve(cwd, 'dist/index.mjs'),
+  readFileSync(resolve(cwd, 'dist/index.d.ts')),
+)
